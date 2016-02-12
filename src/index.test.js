157868fc49a650175014f1b0cbae5d7e5a040c4e
unknown <manuel.de.leon@MW7CDTPCX98PZD.dir.svc.accenture.com>
2016-02-12 09:28:32 -0700

    feat(Use only one !): No need for multiple !
    Oops

--- src/index.test.js
+++ src/index.test.js
@@ -2,8 +2,8 @@ var assert = require("chai").assert;
 var vmc2jsx = require("./index");
 
 describe("vmc2jsx", function() {
-  it("returns string plus !!", function() {
+  it("returns string plus !", function() {
     var result = vmc2jsx.convert("AA");
-    assert.equal(result, "AA!!");
+    assert.equal(result, "AA!");
   })
 });
