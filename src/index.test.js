38e5b0d9fba55e9478554fe758eb7431bc9ab3ae
unknown <manuel.de.leon@MW7CDTPCX98PZD.dir.svc.accenture.com>
2016-02-12 09:23:08 -0700

    chore(releasing): Add travis config, conventional commit, and semantic-release
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var assert = require("chai").assert;
+var vmc2jsx = require("./index");
+
+describe("vmc2jsx", function() {
+  it("returns string plus !!", function() {
+    var result = vmc2jsx.convert("AA");
+    assert.equal(result, "AA!!");
+  })
+});
